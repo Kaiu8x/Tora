b43350b97f71b6225ca91ee93e15ed740835fb63
Kaiu8x <kawaswim@gmail.com>
2018-08-26 11:47:59 -0500

    first commit

---
+++ app/src/main/java/com/example/migue/toraapp/display_messages.java
@@ -0,0 +1,13 @@
+package com.example.migue.toraapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class display_messages extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_messages);
+    }
+}
